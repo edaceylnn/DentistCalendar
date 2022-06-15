38436a37fe14e71a948d743bae0c0d2c14eb8557
edaceylnn <62337318+edaceylnn@users.noreply.github.com>
2022-06-16 01:21:21 +0300

    calendar

---
+++ DentistCalendar/DentistCalendar/Migrations/20220615163751_CreateColumn.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DentistCalendar.Migrations
+{
+    public partial class CreateColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
